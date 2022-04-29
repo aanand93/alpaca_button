d8464ce1761a76186e3bfe148a9b09513429cc7a
Alexander Anand <77586218+aanand93@users.noreply.github.com>
2022-04-29 12:48:18 -0400

    update version 1 of alpaca buttton

--- src/Components/AlpacaButton.js
+++ src/Components/AlpacaButton.js
@@ -1,32 +1,48 @@
 import React from 'react';
-import { keyframes } from 'styled-components';
-import styled from 'styled-components';
+import './AlpacaButton.css';
+import logo2 from '../Alpacas/Alpaca2.jpeg';
+import logo3 from '../Alpacas/Alpaca3.jpeg';
+import logo4 from '../Alpacas/Alpaca4.jpeg';
+import logo6 from '../Alpacas/Alpaca6.jpeg';
+import logo7 from '../Alpacas/Alpaca7.jpeg';
+import logo8 from '../Alpacas/Alpaca8.jpeg';
+import logo9 from '../Alpacas/Alpaca9.jpeg';
+import logo10 from '../Alpacas/Alpaca10.jpeg';
+
+const STYLES = ['btn--primary--solid', 'btn--success--solid'];
+
+const SIZES = ['btn--medium', 'btn--small'];
 
 const AlpacaButton = ({ children, type, onClick, buttonStyle, buttonSize }) => {
-	const rotate = keyframes`
-        from {
-            transform: rotate(0deg);
-        }
+	const checkButtonStyle = STYLES.includes(buttonStyle)
+		? buttonStyle
+		: STYLES[0];
 
-        to {
-            transform: rotate(360deg);
-        }
-    `;
+	const checkButtonSize = STYLES.includes(buttonSize) ? buttonSize : SIZES[0];
 
-	const Rotate = styled.div`
-		display: inline-block;
-		animation: ${rotate} 4s linear infinite;
-		padding: 2rem 1rem;
-		font-size: 1.2rem;
-	`;
+	var newPic = {};
+    var img = document.querySelector('img')
+
+	const alpacaArray = [logo2, logo3, logo4, logo6, logo7, logo8, logo9, logo10];
+	const alpacaClick = (e) => {
+		e.preventDefault();
+		const newPic = alpacaArray[Math.floor(Math.random() * alpacaArray.length)];
+
+        img.src = newPic
+		return console.log(newPic);
+	};
 
 	return (
-		<div>
-			<Rotate>
-				<button onClick={onClick} type={type}>
-					{children}
-				</button>
-			</Rotate>
+		<div className='AlpacaButton'>
+			<button
+				className={`btn ${checkButtonStyle} ${checkButtonSize}`}
+				onClick={alpacaClick}
+				type={type}>
+				{children}
+			</button>
+			<div className='images'>
+				<img id='img' src={logo2} alt='alpaca' />
+			</div>
 		</div>
 	);
 };
